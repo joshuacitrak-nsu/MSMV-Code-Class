19da8a783f6890b9d41c0e8fff7386cd10564136
Joshua Citrak <116121448+joshuacitrak-nsu@users.noreply.github.com>
2025-11-11 19:44:27 +0000

    added 1 warm up + launch files

---
+++ Day-1/Warm-up/01-comment-to-code/script.js
@@ -0,0 +1,15 @@
+// TODO: Get the score display element and the button element from the page.
+const scoreDisplay = document.getElementById("score");
+const clickButton = document.getElementById("clickButton");
+
+// We will keep track of the score in this variable.
+let score = 0;
+
+// TODO: Write the addScore() function.
+// It should: (1) increase score by 1, (2) update the scoreDisplay to "Score: X".
+function addScore() {
+  // your code here
+}
+
+// TODO: When the button is clicked, run addScore().
+clickButton.addEventListener("click", addScore);

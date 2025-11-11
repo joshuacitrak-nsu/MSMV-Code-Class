2dc554dfb62e6f2d715eeb05924514ec7c8bd63c
Joshua Citrak <116121448+joshuacitrak-nsu@users.noreply.github.com>
2025-11-11 14:47:44 -0500

    Wire up button click event and status update
    Implemented event handling for button click.

--- Day-1/Warm-up/02-wire-the-button/script.js
+++ Day-1/Warm-up/02-wire-the-button/script.js
@@ -1,1 +1,17 @@
+// GOAL: Use the comments to wire up the button.
+// 1) Grab the p#status and button#go
+// 2) Make a function called handleClick that sets the status to "clicked!"
+// 3) Add a click event listener to the button to run handleClick
+
+// 1) get elements
+// const statusEl = ...
+// const goBtn = ...
+
+// 2) function
+// function handleClick() {
+//   // update the status text here
+// }
+
+// 3) event listener
+// goBtn.addEventListener( ??? , ??? );
 
